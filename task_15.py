efa8b1fd6b73a05a7635b2d6d1ee9ff8513d8fbe
IslomboevBotir <islomboev_botir@mail.ru>
2023-03-21 16:42:31 +0300

    task_16

--- task_15.py
+++ task_15.py
@@ -2,9 +2,28 @@
 
 from pyrob.api import *
 
-
 @task
 def task_8_21():
-    mov(8, 8)
+    if wall_is_above() and wall_is_on_the_left():
+        while wall_is_on_the_right() == False:
+            move_right()
+        while wall_is_beneath() == False:
+            move_down()
+    elif wall_is_above() and wall_is_on_the_right():
+        pass
+        while wall_is_on_the_left() == False:
+            move_left()
+        while wall_is_beneath() == False:
+            move_down()
+    elif wall_is_beneath() and wall_is_on_the_left():
+        while wall_is_on_the_right() == False:
+            move_right()
+        while wall_is_above() == False:
+            move_up()
+    else:
+        while wall_is_on_the_left() == False:
+            move_left()
+        while wall_is_above() == False:
+            move_up()
 if __name__ == '__main__':    
     run_tasks()
--- task_16.py
+++ task_16.py
@@ -5,8 +5,16 @@ from pyrob.api import *
 
 @task
 def task_8_22():
-    pass
-
+    while wall_is_on_the_left() == True and wall_is_on_the_right():
+        move_up()
+    if wall_is_on_the_left():
+        while wall_is_on_the_right() == False:
+            move_right()
+        return
+    if wall_is_on_the_right():
+        while wall_is_on_the_left() == False:
+            move_left()
+        return
 
 if __name__ == '__main__':
     run_tasks()
