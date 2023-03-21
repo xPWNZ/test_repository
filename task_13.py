d3131da2df562a3c98dfeb9b69a9e36ee07c4841
IslomboevBotir <islomboev_botir@mail.ru>
2023-03-21 13:31:27 +0300

    wall its ok

--- task_13.py
+++ task_13.py
@@ -3,34 +3,95 @@
 from pyrob.api import *
 
 
-def cell_box():
-    if wall_is_above() == False and wall_is_beneath() == False or cell_is_filled() == True:
-        fill_cell()
-
-
-def run_right():
-    cell_box()
-    while wall_is_on_the_right() == False:
-        move_right()
-        cell_box()
-
-
-def run_left():
-    while wall_is_on_the_right == True:
-        move_left()
-
-
-def up_down():
-    if wall_is_above() == True or wall_is_beneath() == False:
-        move_down()
-    elif wall_is_above() == False or wall_is_beneath() == True:
-        move_up()
-
-
 @task
 def task_8_10():
-    up_down()
-
-
+    if wall_is_above() == True and wall_is_beneath() == True:
+        while wall_is_above() == True:
+            move_right()
+        move_up()
+        while wall_is_on_the_right() == False:
+            move_right()
+        if wall_is_on_the_right() == True:
+            move_left()
+            while wall_is_beneath() == True:
+                move_left()
+            if wall_is_beneath() == False:
+                move_down()
+                while wall_is_beneath() == True:
+                    move_left()
+                move_down()
+            while wall_is_on_the_right() == False:
+                move_right()
+            while wall_is_on_the_left() == False:
+                move_left()
+            while wall_is_above() == True:
+                move_right()
+            move_up()
+            while wall_is_on_the_right() == False:
+                move_right()
+    elif wall_is_above() == True and wall_is_beneath() == False:
+        move_down()
+        while wall_is_on_the_right() == False:
+            move_right()
+        if wall_is_on_the_right() == True:
+            move_left
+            while wall_is_above() == True:
+                move_left()
+            move_up()
+            while wall_is_above() == True:
+                move_left()
+            move_up()
+            while wall_is_on_the_right() == False:
+                move_right()
+            while wall_is_on_the_left() == False:
+                move_left()
+            while wall_is_beneath() == True:
+                move_right()
+            move_down()
+            while wall_is_on_the_right() == False:
+                move_right()
+    elif wall_is_above() == False and wall_is_beneath() == True:
+        move_up()
+        while wall_is_on_the_right() == False:
+            move_right()
+        if wall_is_on_the_right() == True:
+            move_left()
+            while wall_is_beneath() == True:
+                move_left()
+            if wall_is_beneath() == False:
+                move_down()
+                while wall_is_beneath() == True:
+                    move_left()
+                move_down()
+            while wall_is_on_the_right() == False:
+                move_right()
+            while wall_is_on_the_left() == False:
+                move_left()
+            while wall_is_above() == True:
+                move_right()
+            move_up()
+            while wall_is_on_the_right() == False:
+                move_right()
+    else:
+        move_up()
+        while wall_is_on_the_right() == False:
+            move_right()
+        if wall_is_on_the_right() == True:
+            move_left()
+            while wall_is_beneath() == True:
+                move_left()
+            move_down() 
+            while wall_is_beneath() == True:
+                move_left()
+            move_down()
+            while wall_is_on_the_right() == False:
+                move_right()
+            while wall_is_on_the_left() == False:
+                move_left()
+            while wall_is_above() == True:
+                move_right() 
+            move_up()
+            while wall_is_on_the_right() == False:
+                move_right()
 if __name__ == '__main__':
     run_tasks()
