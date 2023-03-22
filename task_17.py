87094ba48578406e100b26088b80052b3e39b894
IslomboevBotir <islomboev_botir@mail.ru>
2023-03-22 11:48:04 +0300

    task_17&18

--- task_17.py
+++ task_17.py
@@ -5,7 +5,12 @@ from pyrob.api import *
 
 @task
 def task_8_27():
-    pass
+    while cell_is_filled() == False:
+        move_up()
+    move_left()
+    if cell_is_filled() == False:
+        move_right(2)
+
 
 
 if __name__ == '__main__':
--- task_18.py
+++ task_18.py
@@ -5,8 +5,14 @@ from pyrob.api import *
 
 @task
 def task_8_28():
-    pass
-
+    while wall_is_beneath() and wall_is_above() and wall_is_on_the_left() == False:
+        move_left()
+    while wall_is_above() and wall_is_beneath() and wall_is_on_the_right() == False:
+        move_right()
+    while wall_is_above() == False:
+        move_up()
+    while wall_is_on_the_left() == False:
+        move_left()    
 
 if __name__ == '__main__':
     run_tasks()
